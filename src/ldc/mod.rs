440963d964dc837c191fded178fc96490fc3c059
nan mu <mu.nan.11@outlook.com>
2023-12-11 02:48:42 +0800

    编写了金属探测的功能

---
+++ src/ldc/mod.rs
@@ -0,0 +1,162 @@
+//! [`embedded-hal`] driver for Texas Instruments (TI) I2C inductance-to-digital converters (LDC): [LDC1312/LDC1314], [LDC1612/LDC1614].
+//!
+//! [`embedded-hal`]: https://docs.rs/embedded-hal
+//! [LDC1312/LDC1314]: https://www.ti.com/lit/ds/symlink/ldc1314.pdf
+//! [LDC1612/LDC1614]: https://www.ti.com/lit/ds/symlink/ldc1614.pdf
+use embedded_hal::blocking::i2c;
+
+mod data;
+pub use data::*;
+
+#[derive(Debug)]
+pub enum Error<BE> {
+    Bus(BE),
+    ConversionUnderRange,
+    ConversionOverRange,
+    ConversionWatchdogTimeout,
+    ConversionAmplitude,
+}
+
+/// TI LDC1x1x driver instance
+pub struct Ldc<I2c> {
+    bus: I2c,
+    adr: u8,
+}
+
+impl<I2c, BE> Ldc<I2c>
+where
+    I2c: i2c::Write<Error = BE> + i2c::WriteRead<Error = BE>,
+{
+    pub fn new(bus: I2c, adr: u8) -> Self {
+        Ldc { bus, adr }
+    }
+
+    pub fn write_reg(&mut self, reg: u8, data: u16) -> Result<(), Error<BE>> {
+        self.bus
+            .write(self.adr, &[reg, (data >> 8) as u8, data as u8])
+            .map_err(Error::Bus)
+    }
+
+    pub fn read_reg(&mut self, reg: u8) -> Result<u16, Error<BE>> {
+        let mut result: [u8; 2] = [0xde, 0xad];
+        self.bus
+            .write_read(self.adr, &[reg], &mut result)
+            .map_err(Error::Bus)?;
+        Ok((result[0] as u16) << 8 | result[1] as u16)
+    }
+
+    /// Read the conversion result for a channel.
+    /// Error flags from the result are returned as errors.
+    /// Reading does clear the error flags on the device.
+    ///
+    /// This function must only be used with 12-bit devices (LDC131x).
+    /// Use read_data_24bit with 24-bit devices (LDC161x).
+    pub fn read_data_12bit(&mut self, ch: Channel) -> Result<u16, Error<BE>> {
+        let b = self.read_reg(2 * ch as u8)?;
+        if b & (1 << 15) != 0 {
+            return Err(Error::ConversionUnderRange);
+        }
+        if b & (1 << 14) != 0 {
+            return Err(Error::ConversionOverRange);
+        }
+        if b & (1 << 13) != 0 {
+            return Err(Error::ConversionWatchdogTimeout);
+        }
+        if b & (1 << 12) != 0 {
+            return Err(Error::ConversionAmplitude);
+        }
+        Ok(b & 0x0fff)
+    }
+
+    /// Read the conversion result for a channel.
+    /// Error flags from the result are returned as errors.
+    /// Reading does clear the error flags on the device.
+    ///
+    /// This function must only be used with 24-bit devices (LDC161x).
+    /// Use read_data_12bit with 12-bit devices (LDC131x).
+    pub fn read_data_24bit(&mut self, ch: Channel) -> Result<u32, Error<BE>> {
+        Ok((self.read_data_12bit(ch)? as u32) << 16 | self.read_reg(1 + 2 * ch as u8)? as u32)
+    }
+
+    pub fn set_ref_count_conv_interval(&mut self, ch: Channel, intv: u16) -> Result<(), Error<BE>> {
+        self.write_reg(0x08 + ch as u8, intv)
+    }
+
+    pub fn ref_count_conv_interval(&mut self, ch: Channel) -> Result<u16, Error<BE>> {
+        self.read_reg(0x08 + ch as u8)
+    }
+
+    pub fn set_conv_offset(&mut self, ch: Channel, offs: u16) -> Result<(), Error<BE>> {
+        self.write_reg(0x0c + ch as u8, offs)
+    }
+
+    pub fn conv_offset(&mut self, ch: Channel) -> Result<u16, Error<BE>> {
+        self.read_reg(0x0c + ch as u8)
+    }
+
+    pub fn set_conv_settling_time(&mut self, ch: Channel, cnt: u16) -> Result<(), Error<BE>> {
+        self.write_reg(0x10 + ch as u8, cnt)
+    }
+
+    pub fn conv_settling_time(&mut self, ch: Channel) -> Result<u16, Error<BE>> {
+        self.read_reg(0x10 + ch as u8)
+    }
+
+    pub fn set_clock_dividers(
+        &mut self,
+        ch: Channel,
+        divs: ClockDividers,
+    ) -> Result<(), Error<BE>> {
+        self.write_reg(0x14 + ch as u8, divs.fin_div << 12 | divs.fref_div)
+    }
+
+    pub fn status(&mut self) -> Result<Status, Error<BE>> {
+        self.read_reg(0x18).map(Status)
+    }
+
+    pub fn error_config(&mut self) -> Result<ErrorConfig, Error<BE>> {
+        self.read_reg(0x19).map(ErrorConfig)
+    }
+
+    pub fn set_error_config(&mut self, conf: ErrorConfig) -> Result<(), Error<BE>> {
+        self.write_reg(0x19, conf.0)
+    }
+
+    pub fn config(&mut self) -> Result<Config, Error<BE>> {
+        self.read_reg(0x1A).map(Config)
+    }
+
+    pub fn set_config(&mut self, conf: Config) -> Result<(), Error<BE>> {
+        self.write_reg(0x1A, conf.0)
+    }
+
+    pub fn mux_config(&mut self) -> Result<MuxConfig, Error<BE>> {
+        self.read_reg(0x1B).map(MuxConfig)
+    }
+
+    pub fn set_mux_config(&mut self, conf: MuxConfig) -> Result<(), Error<BE>> {
+        self.write_reg(0x1B, conf.0)
+    }
+
+    pub fn reset(&mut self) -> Result<(), Error<BE>> {
+        self.write_reg(0x1C, 1 << 15)
+    }
+
+    // TODO: 131x also have a gain field in the reset register
+
+    pub fn set_sensor_drive_current(&mut self, ch: Channel, cur: u8) -> Result<(), Error<BE>> {
+        self.write_reg(0x1E + ch as u8, (cur as u16) << 11)
+    }
+
+    pub fn measured_sensor_drive_current(&mut self, ch: Channel) -> Result<u8, Error<BE>> {
+        Ok(((self.read_reg(0x1E + ch as u8)? >> 6) & 0b11111) as u8)
+    }
+
+    pub fn manufacturer_id(&mut self) -> Result<u16, Error<BE>> {
+        self.read_reg(0x7E)
+    }
+
+    pub fn device_id(&mut self) -> Result<u16, Error<BE>> {
+        self.read_reg(0x7F)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,18 @@
-//! Draw a 1 bit per pixel black and white image. On a 128x64 SSD1306 display over I2C.
-//!
-//! Image was created with ImageMagick:
-//!
-//! ```bash
-//! convert rust.png -depth 1 gray:rust.raw
-//! ```
-//!
-//! This example is for the STM32F103 "Blue Pill" board using I2C1.
-//!
-//! Wiring connections are as follows for a CRIUS-branded display:
-//!
-//! ```
-//!      Display -> Blue Pill
-//! (black)  GND -> GND
-//! (red)    +5V -> VCC
-//! (yellow) SDA -> PB9
-//! (green)  SCL -> PB8
-//! ```
-//!
-//! Run on a Blue Pill with `cargo run --example image_i2c`.
-
 #![no_std]
 #![no_main]
+#![feature(const_float_bits_conv)]
+#![feature(const_fn_floating_point_arithmetic)]
 
-use cortex_m_rt::{entry, exception, ExceptionFrame};
+extern crate alloc;
+use alloc::string::ToString;
+use cortex_m_rt::entry;
 use embedded_graphics::{
-    image::{Image, ImageRaw},
+    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
     pixelcolor::BinaryColor,
     prelude::*,
+    text::{Baseline, Text},
 };
+mod ldc;
 use panic_halt as _;
 use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
 use stm32f1xx_hal::{
@@ -37,25 +21,26 @@ use stm32f1xx_hal::{
     stm32,
 };
 
+use embedded_alloc::Heap;
+
+#[global_allocator]
+static HEAP: Heap = Heap::empty();
+
 #[entry]
 fn main() -> ! {
     let dp = stm32::Peripherals::take().unwrap();
-
     let mut flash = dp.FLASH.constrain();
     let rcc = dp.RCC.constrain();
-
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
-
     let mut afio = dp.AFIO.constrain();
-
     let mut gpiob = dp.GPIOB.split();
 
-    let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
-    let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
-
-    let i2c = BlockingI2c::i2c1(
+    let i2c1 = BlockingI2c::i2c1(
         dp.I2C1,
-        (scl, sda),
+        (
+            gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh),
+            gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh),
+        ),
         &mut afio.mapr,
         Mode::Fast {
             frequency: 400_000.Hz(),
@@ -67,24 +52,65 @@ fn main() -> ! {
         1000,
         1000,
     );
+    let i2c2 = BlockingI2c::i2c2(
+        dp.I2C2,
+        (
+            gpiob.pb10.into_alternate_open_drain(&mut gpiob.crh),
+            gpiob.pb11.into_alternate_open_drain(&mut gpiob.crh),
+        ),
+        Mode::Fast {
+            frequency: 400_000.Hz(),
+            duty_cycle: DutyCycle::Ratio2to1,
+        },
+        clocks,
+        1000,
+        10,
+        1000,
+        1000,
+    );
 
-    let interface = I2CDisplayInterface::new(i2c);
+    let interface = I2CDisplayInterface::new(i2c2);
     let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
         .into_buffered_graphics_mode();
     display.init().unwrap();
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X10)
+        .text_color(BinaryColor::On)
+        .build();
 
-    let raw: ImageRaw<BinaryColor> = ImageRaw::new(include_bytes!("./rust.raw"), 64);
+    let adr = u8::from_str_radix("2b", 16).unwrap(); // I2C 地址
+    let mut ldc = ldc::Ldc::new(i2c1, adr);
+    ldc.reset().unwrap();
+    let div = ldc::Fsensor::from_inductance_capacitance(19.0, 100.0).to_clock_dividers(None);
+    ldc.set_clock_dividers(ldc::Channel::One, div).unwrap();
+    ldc.set_conv_settling_time(ldc::Channel::One, 40).unwrap();
+    ldc.set_ref_count_conv_interval(ldc::Channel::One, 0x0546)
+        .unwrap();
+    ldc.set_sensor_drive_current(ldc::Channel::One, 0b01110)
+        .unwrap();
+    ldc.set_mux_config(
+        ldc::MuxConfig::default()
+            .with_auto_scan(true)
+            .with_deglitch_filter_bandwidth(ldc::Deglitch::ThreePointThreeMHz),
+    )
+    .unwrap();
+    ldc.set_config(ldc::Config::default()).unwrap();
+    ldc.set_error_config(
+        ldc::ErrorConfig::default().with_amplitude_high_error_to_data_register(true),
+    )
+    .unwrap();
 
-    let im = Image::new(&raw, Point::new(32, 0));
-
-    im.draw(&mut display).unwrap();
-
-    display.flush().unwrap();
-
-    loop {}
-}
-
-#[exception]
-unsafe fn HardFault(ef: &ExceptionFrame) -> ! {
-    panic!("{:#?}", ef);
+    // timing ignored because polling with a cp2112 with no delays is slow enough already
+    // outputting just newline separated numbers so you can feed it into https://github.com/mogenson/ploot
+    loop {
+        Text::with_baseline(
+            &ldc.read_data_24bit(ldc::Channel::Zero).unwrap().to_string(),
+            Point::new(0, 16),
+            text_style,
+            Baseline::Top,
+        )
+        .draw(&mut display)
+        .unwrap();
+        display.flush().unwrap();
+    }
 }
