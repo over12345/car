0c04cf118aeff6870c31c26340fd9f1395aee0d8
nan mu <mu.nan.11@outlook.com>
2023-12-20 11:33:57 +0800

    还是不换了，其他更烂，stm32f1系列已经被社区抛弃了，是个教训，应该买f4系列

--- src/init.rs
+++ src/init.rs
@@ -1,8 +1,12 @@
+use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::PwmPin;
 use fugit::RateExtU32;
 use stm32f1xx_hal::afio::AfioExt;
 use stm32f1xx_hal::flash::FlashExt;
 use stm32f1xx_hal::gpio::GpioExt;
+use stm32f1xx_hal::pac::TIM2;
 use stm32f1xx_hal::prelude::_stm32_hal_rcc_RccExt;
+use stm32f1xx_hal::timer::PwmChannel;
 use stm32f1xx_hal::timer::Tim2NoRemap;
 use stm32f1xx_hal::timer::Timer;
 /// 这里讲包含所有需要引脚的初始话和定义调度器结构体
@@ -12,10 +16,10 @@ struct CarPins {
     motor: Tb6612fng<
         gpio::Pin<'A', 10, gpio::Output>,
         gpio::Pin<'A', 11, gpio::Output>,
-        _,
+        PwmChannel<TIM2, 0>,
         gpio::Pin<'A', 12, gpio::Output>,
         gpio::Pin<'A', 9, gpio::Output>,
-        _,
+        PwmChannel<TIM2, 1>,
         gpio::Pin<'A', 3, gpio::Output>,
     >,
 }
@@ -36,12 +40,11 @@ impl CarPins {
             gpioa.pa1.into_alternate_push_pull(&mut gpioa.crl),
         );
         let mut afio = dp.AFIO.constrain();
-        let (pwm_0, pwm_1) = Timer::new(dp.TIM2, &clocks).pwm_hz::<Tim2NoRemap, _, _>(
-            pins_pwm,
-            &mut afio.mapr,
-            100.Hz(),
-        );
+        let (mut pwm_0, mut pwm_1) = Timer::new(dp.TIM2, &clocks)
+            .pwm_hz::<Tim2NoRemap, _, _>(pins_pwm, &mut afio.mapr, 100.Hz())
+            .split();
         pwm_0.set_duty(50);
+        pwm_1.set_duty(50);
         let motor_a_in1 = gpioa.pa10.into_push_pull_output(&mut gpioa.crh); //电机A的输入信号1的引脚与pa0相连，用的是tim2的通道1
         let motor_a_in2 = gpioa.pa11.into_push_pull_output(&mut gpioa.crh); //电机A的输入信号2的引脚与pa1相连，用的是tim2的通道2
         let motor_b_in1 = gpioa.pa12.into_push_pull_output(&mut gpioa.crh); //电机B的输入信号1的引脚与pa2相连，用的是tim2的通道3
